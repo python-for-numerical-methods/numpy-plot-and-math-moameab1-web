710dfac84f3ca745a3e7a4557b2fd7b7e8dcd875
moameab1-web <moameab1@ac.sce.ac.il>
2026-04-16 21:20:04 +0300

    Update normalized_array.py

--- normalized_array.py
+++ normalized_array.py
@@ -1,24 +1,9 @@
 import numpy as np
 
 def normalized_array(data):
-    """
-    מנרמלת מערך נתונים לטווח של [0, 1] לפי שיטת Min-Max Scaling.
-    
-    הנוסחה לביצוע:
-    x_norm = (x - min) / (max - min)
-    
-    פרמטרים:
-    data (list or np.array): מערך של מספרים.
-    
-    מחזירה:
-    np.array: מערך מנורמל. אם כל הערכים במערך זהים, יש להחזיר מערך של אפסים.
-    """
-    # המרת הקלט ל-numpy array לצורך חישובים וקטוריים
-    data = np.array(data)
-    
-    # --- כיתבו את הקוד שלכם כאן ---
-    pass
-    # חשוב לזכור להחליף את pass ב- return
+    new_array=np.array(data)
+    new_array = (new_array - np.min(new_array)) / (np.max(new_array) - np.min(new_array))
+    return new_array
 
 if __name__ == "__main__":
     # כאן הסטודנטים יכולים להריץ בדיקה עצמית מהירה
